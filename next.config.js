14310b62353d7969466f8edae020fd0c187e8548
rrnoa <roiky82@gmail.com>
2023-10-18 17:08:34 -0400

    path change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+//const isProd = (process.env.NODE_ENV = "development");
+
 const nextConfig = {
+  basePath: "/lignumNext",
+  //basePath: isProd ? "/lignumNext" : "",
   output: "export", //para que produsca paginas estaticas
   images: { unoptimized: true },
 };
