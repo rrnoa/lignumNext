61606bf06d3b1c03f3dad831ca90f9e2be9547d0
rrnoa <roiky82@gmail.com>
2023-10-18 17:53:20 -0400

    cambiando export nex.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 const nextConfig = {
   basePath: "/lignumNext",
   //basePath: isProd ? "/lignumNext" : "",
-  //output: "export", //para que produsca paginas estaticas
+  output: "export", //para que produsca paginas estaticas
   //images: { unoptimized: true },
 };
 module.exports = nextConfig;
